2cb48ff6e78eb5052e40639482464fa9f05c3828
2xandrade <r.2x.andrade@gmail.com>
2025-06-07 15:43:02 -0300

    Update preloadAssets.js

--- Game/scenes/preloadAssets.js
+++ Game/scenes/preloadAssets.js
@@ -17,7 +17,7 @@ class PreloadAssets extends Phaser.Scene {
         // NOTE: These asset paths point to files not included in the blog post code.
         // You will need to provide these image files in an 'assets/sprites/' directory.
         this.load.image('enemy', 'assets/sprites/enemy.png');      // the big circle
-        this.load.image('player', 'assets/sprites/player.png');    // the player
+        this.load.image('player', 'assets/sprites/paladinoFrenteParado.png');    // the player
         this.load.image('bullet', 'assets/sprites/bullet.png');    // the spike
         this.load.image('coin', 'assets/sprites/coin.png');
         this.load.image('tiles', 'assets/sprites/grassy_field.png');
